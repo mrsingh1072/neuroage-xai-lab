a2397b8f620dad9a6047ea48eeff86a5af0e8123
Saurabh singh <saurabh_kumar@srmap.edu.in>
2026-04-04 23:54:58 +0530

    chnage

--- frontend/src/components/FormInput.jsx
+++ frontend/src/components/FormInput.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react';
 import { Eye, EyeOff } from 'lucide-react';
 import { motion } from 'framer-motion';
 
+
+
 export default function FormInput({
   label,
   type = 'text',
